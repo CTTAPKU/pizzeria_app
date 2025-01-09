0878862010c04349789db8cd6bbd65bea319279d
Klunnyk Dmytro <117164181+CTTAPKU@users.noreply.github.com>
2025-01-09 14:14:42 +0200

    order changes

--- lib/Services/menu_data.dart
+++ lib/Services/menu_data.dart
@@ -1,10 +1,16 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class MenuData {
-
   final FirebaseFirestore firestoreInstance = FirebaseFirestore.instance;
 
-  Future<void> setPizza(String pizzaName, String pizzaSize, String pizzaPrice, String pizzaDescription, String pizzaComposition, String pictureURL) async {
+  Future<void> setPizza(
+      String pizzaName,
+      String pizzaSize,
+      String pizzaPrice,
+      String pizzaDescription,
+      String pizzaComposition,
+      String pictureURL,
+      List<String> extraIngredients) async {
     DocumentReference newCityRef = firestoreInstance.collection("Pizzas").doc();
     newCityRef.set({
       'pizzaName': pizzaName,
@@ -12,6 +18,7 @@ class MenuData {
       "pizzaPrice": pizzaPrice,
       "pizzaDescription": pizzaDescription,
       "pizzaComposition": pizzaComposition,
+      "extraIngredients": extraIngredients,
       "pictureURL": pictureURL,
       "id": newCityRef.id
     });
@@ -28,7 +35,10 @@ class MenuData {
   }
 
   Future<Map<String, dynamic>> getPizzaData(String id) async {
-    final pizza = await firestoreInstance.collection('Pizzas').where("id", isEqualTo: id).get();
+    final pizza = await firestoreInstance
+        .collection('Pizzas')
+        .where("id", isEqualTo: id)
+        .get();
     return pizza.docs.first.data();
   }
 }
